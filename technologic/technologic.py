557cc227a3bc7ec587b1cafc0c3ada914902cd79
Brian Vallelunga <bvallelunga@parkme.com>
2012-07-21 03:27:41 -0700

    Update

--- technologic/technologic.py
+++ technologic/technologic.py
@@ -1,3 +1,11 @@
-__author__ = 'Brian'
+'''
+This system will create music based off of y=mx+b equations.
+There are two sets of equations:
+    1) x is time and y is audio sound (higher y = high pitch) (low y = big bass)
+    2) x is time and y is the duration of the sound in set 1 (may have 1+ equations > piecewise function)
+    BOTH sets will stop at the duration. Duration in seconds
+'''
+def logic(type, duration):
+    print duration
 
-
+logic(5)
