1028fa34ac55a9b720c7c36afa569be00cd874e3
Brian Vallelunga <brian@laborate.io>
2014-01-19 03:57:10 -0800

    test

--- synthesize.py
+++ synthesize.py
@@ -1,5 +1,5 @@
 """
-# made in laborate (http://laborate.io) (http://laborate.io)
+# made in laborate [http://laborate.io] (http://laborate.io)
 # 'song' is a Python list (or tuple) in which the song is defined,
 #   the format is [['note', value]]
 
